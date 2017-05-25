6bd5e636df378c6fe1cd242377cdd2e08be41891
mars <mars@huoban.com>
2017-05-25 15:21:04 +0800

    对齐了。。

--- inherit.js
+++ inherit.js
@@ -133,3 +133,220 @@ F.prototype.compareTo = function(that) {
   return this.value - that.value
 }
 
+function defineSubclass(superclass, constructor, methods, statics) {
+  constructor.prototype = inherit(superclass.prototype)
+  constructor.prototype.constructor = constructor
+  if (methods) extend(constructor.prototype, methods)
+  if (statics) extend(constructor, statics)
+  return constructor
+}
+
+if (!Function.prototype.softBind) {
+  Function.prototype.softBind = function(obj) {
+    var fn = this,
+      curried = [].slice.call( arguments, 1 ),
+      bound = function bound() {
+        return fn.apply(
+          (!this ||
+            (typeof window !== "undefined" &&
+              this === window) ||
+            (typeof global !== "undefined" &&
+              this === global)
+          ) ? obj : this,
+          curried.concat.apply( curried, arguments )
+        );
+      };
+    bound.prototype = Object.create( fn.prototype );
+    return bound;
+  };
+}
+
+// iterator
+
+var myObject = {
+  a: 2,
+  b: 3
+};
+
+Object.defineProperty( myObject, Symbol.iterator, {
+  enumerable: false,
+  writable: false,
+  configurable: true,
+  value: function() {
+    var o = this;
+    var idx = 0;
+    var ks = Object.keys( o );
+    return {
+      next: function() {
+        return {
+          value: o[ks[idx++]],
+          done: (idx > ks.length)
+        };
+      }
+    };
+  }
+} );
+
+// iterate `myObject` manually
+var it = myObject[Symbol.iterator]();
+it.next(); // { value:2, done:false }
+it.next(); // { value:3, done:false }
+it.next(); // { value:undefined, done:true }
+
+///
+
+if (!Object.create) {
+  Object.create = function(o) {
+    function F(){}
+    F.prototype = o;
+    return new F();
+  };
+}
+
+// inherit test OO style
+
+function Foo(who) {
+  this.me = who;
+}
+Foo.prototype.identify = function() {
+  return "I am " + this.me;
+};
+
+function Bar(who) {
+  Foo.call( this, who );
+}
+Bar.prototype = Object.create( Foo.prototype );
+
+Bar.prototype.speak = function() {
+  alert( "Hello, " + this.identify() + "." );
+};
+
+var b1 = new Bar( "b1" );
+var b2 = new Bar( "b2" );
+
+b1.speak();
+b2.speak();
+
+// OLOO style
+
+var Foo = {
+  init: function(who) {
+    this.me = who;
+  },
+  identify: function() {
+    return "I am " + this.me;
+  }
+};
+
+var Bar = Object.create( Foo );
+
+Bar.speak = function() {
+  alert( "Hello, " + this.identify() + "." );
+};
+
+var b1 = Object.create( Bar );
+b1.init( "b1" );
+var b2 = Object.create( Bar );
+b2.init( "b2" );
+
+b1.speak();
+b2.speak();
+
+// class style
+// Parent class
+function Widget(width,height) {
+  this.width = width || 50;
+  this.height = height || 50;
+  this.$elem = null;
+}
+
+Widget.prototype.render = function($where){
+  if (this.$elem) {
+    this.$elem.css( {
+      width: this.width + "px",
+      height: this.height + "px"
+    } ).appendTo( $where );
+  }
+};
+
+// Child class
+function Button(width,height,label) {
+  // "super" constructor call
+  Widget.call( this, width, height );
+  this.label = label || "Default";
+
+  this.$elem = $( "<button>" ).text( this.label );
+}
+
+// make `Button` "inherit" from `Widget`
+Button.prototype = Object.create( Widget.prototype );
+
+// override base "inherited" `render(..)`
+Button.prototype.render = function($where) {
+  // "super" call
+  Widget.prototype.render.call( this, $where );
+  this.$elem.click( this.onClick.bind( this ) );
+};
+
+Button.prototype.onClick = function(evt) {
+  console.log( "Button '" + this.label + "' clicked!" );
+};
+
+$( document ).ready( function(){
+  var $body = $( document.body );
+  var btn1 = new Button( 125, 30, "Hello" );
+  var btn2 = new Button( 150, 40, "World" );
+
+  btn1.render( $body );
+  btn2.render( $body );
+} );
+
+// delegating style / oloo
+
+var Widget = {
+  init: function(width,height){
+    this.width = width || 50;
+    this.height = height || 50;
+    this.$elem = null;
+  },
+  insert: function($where){
+    if (this.$elem) {
+      this.$elem.css( {
+        width: this.width + "px",
+        height: this.height + "px"
+      } ).appendTo( $where );
+    }
+  }
+};
+
+var Button = Object.create( Widget );
+
+Button.setup = function(width,height,label){
+  // delegated call
+  this.init( width, height );
+  this.label = label || "Default";
+
+  this.$elem = $( "<button>" ).text( this.label );
+};
+Button.build = function($where) {
+  // delegated call
+  this.insert( $where );
+  this.$elem.click( this.onClick.bind( this ) );
+};
+Button.onClick = function(evt) {
+  console.log( "Button '" + this.label + "' clicked!" );
+};
+
+$( document ).ready( function(){
+  var $body = $( document.body );
+
+  var btn1 = Object.create( Button );
+  btn1.setup( 125, 30, "Hello" );
+
+  var btn2 = Object.create( Button );
+  btn2.setup( 150, 40, "World" );
+
+  btn1.build( $body );
+  btn2.build( $body );
+} );
+
