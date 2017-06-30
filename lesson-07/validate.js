d51bcdfd598305e7862a4a842e5f4f49223f7f91
mars <mars@huoban.com>
2017-06-30 22:54:36 +0800

    补完

--- lesson-07/validate.js
+++ lesson-07/validate.js
@@ -1,73 +1,67 @@
 function validateUsername() {
-  // return Math.random() > 0.5 ? '未填写' : true
+  // 用户名规则：包含中文，数字，英文，下划线，长度4-20
+  var regx = /^[\u4E00-\u9FA5A-Za-z0-9_]{4,20}$/
+  // your code is here
   return true
 }
 
 function validatePassword() {
-  // return '未填写'
+  // 密码必须包含数字、大小写英文、!@#三个中任何一个符号，长度8-24个字符
+  // 比如 '123aaaAA' 不符合，'123aaaA!' 符合
+  var regx = /^(?=.*[\!\@\#])(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).{8,24}$/
+  // your code is here
   return true
 }
 
 function validateEmail() {
-  // return '未填写'
+  var input = document.querySelector('input[name=email]')
+  var value = input.value
+  if (!value.trim()) {
+    return '请填写email'
+  }
+  var regx = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+  if (!value.trim().match(regx)) {
+    return '请填写正确的email格式'
+  }
+
   return true
 }
 
 function validateGender() {
-  var elements = document.querySelectorAll('input[name=gender]')
-  for(var i=0; i< elements.length; i++ ){
-    var element = elements[i]
-    if (element.checked) {
-      return true
-    }
+  var element = document.querySelector('input[name=gender]:checked')
+  if (!!element) {
+    return true
   }
   return '请填写性别'
 }
 
 function validateForm() {
-  var names = [
+  var fieldNames = [
     'username', 'password', 'email', 'gender'
   ]
-  var status = []
+  var validateResults = []
+  validateResults[0] = validateUsername()
+  validateResults[1] = validatePassword()
+  validateResults[2] = validateEmail()
+  validateResults[3] = validateGender()
 
-  status[0] = validateUsername()
-  status[1] = validatePassword()
-  status[2] = validateEmail()
-  status[3] = validateGender()
-  console.log(status)
-  var success = true
-  for(var i=0; i < status.length; i++) {
-    var current
-    var name = names[i]
-    var div = document.getElementById(name + '-container')
+  var flag = true
+  for (var i=0; i<validateResults.length; i++) {
+    var success = validateResults[i] === true
+    var fieldName = fieldNames[i]
+    var div = document.getElementById(fieldName + '-container')
     var span = div.querySelector('.tips')
-
-    if (status[i] === true) {
-      current = true
-      div.classList.remove('has-error')
-      span.innerText = ''
-    } else {
-      current = false
+    if (!success) {
+      span.innerText = validateResults[i]
       div.classList.add('has-error')
-      span.innerText = status[i]
+    } else {
+      span.innerText = ''
+      div.classList.remove('has-error')
     }
-    success = current && success
+    flag = flag && success
   }
 
+  // return flag
   return false
-
-  // {
-  //   username: true,
-  //   password: true,
-  //   email: true,
-  //   gender: true,
-  // }
-
-  // {
-  //   username: '未填写',
-  //   password: true,
-  //   email: true,
-  //   gender: true,
-  // }
 }
 
