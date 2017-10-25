5325a5a8ad23f105af51110e945cec59a81a5e4d
mars <mars@huoban.com>
2017-10-25 13:47:59 +0800

    add 完美法

--- lesson-18/answer.js
+++ lesson-18/answer.js
@@ -78,3 +78,8 @@ function f5(n) {
   return c;
 }
 
+// 完美法
+function f6(n) {
+  const tmp = n - ((n >> 1) & 033333333333) - ((n >> 2) & 011111111111);
+  return ((tmp + (tmp >> 3)) & 030707070707) % 63;
+}
