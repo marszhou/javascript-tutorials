ae56eb9e5fc27aa5fbf560297336e21573977527
mars <mars@huoban.com>
2017-11-28 15:03:40 +0800

    入口

---
+++ lesson-21/calc/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Calculator from 'Calculator'
+const App = () => {
+  return (
+    <div>
+      <Calculator />
+    </div>
+  );
+};
+
+export default App;
---
+++ lesson-21/calc/src/Calculator.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Calculator = () => {
+  return (
+    <div>
+      Calculator
+    </div>
+  );
+};
+
+export default Calculator;
