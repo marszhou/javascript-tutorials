79cf406496447541949a16eb2342b69a8a83c314
mars <mars@huoban.com>
2017-11-28 17:05:30 +0800

    render buttons

--- lesson-21/calc/src/Calculator/Calculator.jsx
+++ lesson-21/calc/src/Calculator/Calculator.jsx
@@ -3,37 +3,54 @@ import './Calculator.css';
 import Display from './Display';
 import Button from './Button';
 
+const buttons = [
+  // type, value, text=value, size=1
+  ['func', 'clear', 'C'],
+  ['func', 'toggle_positive', '±'],
+  ['func', 'percent', '%'],
+  ['operator', 'divide', '÷'],
+  ['number', 7],
+  ['number', 8],
+  ['number', 9],
+  ['operator', 'multiply', '×'],
+  ['number', 4],
+  ['number', 5],
+  ['number', 6],
+  ['operator', 'minus', '-'],
+  ['number', 1],
+  ['number', 2],
+  ['number', 3],
+  ['operator', 'plus', '+'],
+  ['number', 0, 0, 2],
+  ['entity', '.'],
+  ['operator', '=']
+];
+
 class Calculator extends React.Component {
   handleButtonClick = (type, value) => {
-    console.log(type, value)
-  }
+    console.log(type, value);
+  };
 
   render() {
     return (
       <ul id="cal">
         <Display />
-        <Button type="func" value="C" onClick={this.handleButtonClick}>C</Button>
-        <li className="key func">±</li>
-        <li className="key func">％</li>
-        <li className="key operator">÷</li>
-        <li className="key number">7</li>
-        <li className="key number">8</li>
-        <li className="key number">9</li>
-        <li className="key operator">×</li>
-        <li className="key number">4</li>
-        <li className="key number">5</li>
-        <li className="key number">6</li>
-        <li className="key operator">-</li>
-        <li className="key number">1</li>
-        <li className="key number">2</li>
-        <li className="key number">3</li>
-        <li className="key operator">+</li>
-        <li className="key size2 number">0</li>
-        <li className="key dot">.</li>
-        <li className="key operator">=</li>
+        {buttons.map(([type, value, text, size = 1]) => {
+          text = text || value;
+          return (
+            <Button
+              key={value}
+              type={type}
+              value={value}
+              size={size}
+              onClick={this.handleButtonClick}
+            >{text}</Button>
+          );
+        })}
+
       </ul>
     );
   }
-};
+}
 
 export default Calculator;
