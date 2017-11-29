7a2d7c361d9d075b74d493257504ac7373462305
mars <mars@huoban.com>
2017-11-29 12:00:44 +0800

    add loading

--- lesson-21/calc/src/App.jsx
+++ lesson-21/calc/src/App.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
-import Calculator from './Calculator/Calculator'
-const App = () => {
-  return (
-    <div>
-      <Calculator />
-    </div>
-  );
-};
+import Calculator from './Calculator/Calculator';
+// const App = () => {
+//   return (
+//     <div>
+//       <Calculator />
+//     </div>
+//   );
+// };
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cssLoaded: false
+    };
+  }
+
+  componentWillMount() {
+    const el = document.querySelector('link[rel="stylesheet"]');
+    el.addEventListener('load', () =>
+      this.setState({
+        cssLoaded: true
+      })
+    );
+  }
+
+  render() {
+    return <div>{!this.state.cssLoaded ? 'loading...' : <Calculator />}</div>;
+  }
+}
 
 export default App;
