2cf66d435e71e32e53706ff31ecb3fbee6084405
sunsy <sunshiyong8340@126.com>
2018-01-02 21:57:50 +0800

    no message

---
+++ lesson-23/sunsy-react-calendar/src/calendar/DatePickerBody.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class DatePickerBody extends Component {
+	render() {
+		return (
+			<div>
+				
+			</div>
+		);
+	}
+}
+
+export default DatePickerBody;
