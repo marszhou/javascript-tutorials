a86f2bb023a495bdee31fb26b389b1ff24f708c2
sysun-PC\sysun <sunshiyong8340@126.com>
2018-01-10 16:23:32 +0800

    no message

--- lesson-23/sunsy-react-calendar/src/calendar/DatePickerBody.jsx
+++ lesson-23/sunsy-react-calendar/src/calendar/DatePickerBody.jsx
@@ -1,11 +1,58 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import DateCell from './DateCell';
+import {getWeeksForDateBody, isWeekend} from './utils';
+import moment from 'moment';
 
 class DatePickerBody extends Component {
+	static propTypes = {
+		start: PropTypes.object, // moment object
+		selected: PropTypes.func, // moment object
+		onSelectDate: PropTypes.func
+	}
+
 	render() {
+		const {
+			start,
+			selected: selectedDate,
+			onSelectDate
+		} = this.props;
+		const weeks = getWeeksForDateBody(start);
+		const current = moment();
+		const compareMonth = start.format('YYYYMM');
+
 		return (
-			<div>
-				
-			</div>
+			<tbody>
+				{weeks.map((days, i) => (
+					<tr key={i}>
+						{days.map((day, k) => {
+							const compareMonth2 = day.format('YYYYMM');
+							const mode = 
+								compareMonth2 > compareMonth
+								? 'new'
+								: compareMonth2 < compareMonth ? 'old' : 'current';
+							const today =
+								day.format('YYYYMMDD') ===current.format('YYYYMMDD')
+							const weekend = isWeekend(day);
+							const selected = selectedDate
+								? selectedDate.format('YYYYMMDD') ===day.format('YYYYMMDD')
+								: false;
+							
+						return (
+							<DateCell 
+								key={k}
+								date={day}
+								mode={mode}
+								today={today}
+								weekend={weekend}
+								selected={selected}
+								onClick={() => onSelectDate(day)}
+							/>
+						)
+						})}
+					</tr>
+				))}
+			</tbody>
 		);
 	}
 }
--- lesson-23/sunsy-react-calendar/src/calendar/DatePickerHeader.jsx
+++ lesson-23/sunsy-react-calendar/src/calendar/DatePickerHeader.jsx
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class DatePickerHeader extends Component {
+	static propTypes = {
+		start: PropTypes.object,
+		onForward: PropTypes.func,
+		onBackward: PropTypes.func,
+		onShowYearPicker: PropTypes.func,
+		onShowMonthPicker: PropTypes.func
+	}
+
 	render() {
+		const {
+			start,
+			onForward,
+			onBackward,
+			onShowYearPicker,
+			onShowMonthPicker
+		} = this.props;
+		const month = start.format('M月');
+		const year = start.format('YYYY年');
+
 		return (
 			<thead>
 				<tr>
-					<th className="prev" data-action="previous">
+					{/*向左箭头*/}
+					<th className="prev" data-action="previous" onClick={onBackward}>
 						<span
 							className="glyphicon glyphicon-chevron-left"
 							title="Previous Month"
 						/>
 					</th>
+					{/*年月显示*/}
 					<th
 						className="picker-switch"
 						data-action="pickerSwitch"
 						colSpan={5}
 						title="Select Month"
 					>
-						<a href=''>December</a> <a href=''>2017</a>
+						<a href="#" onClick={onShowYearPicker}>
+							{year}
+						</a>
+							{' '}
+						<a href="#" onClick={onShowMonthPicker}>
+							{month}
+						</a>
 					</th>
-					<th className="next" data-action="next">
+					{/*向右箭头*/}
+					<th className="next" data-action="next" onClick={onForward}>
 						<span
 							className="glyphicon glyphicon-chevron-right"
 							title="Next Month"
@@ -27,13 +55,13 @@ class DatePickerHeader extends Component {
 					</th>
 				</tr>
 				<tr>
-				<th className="dow">Su</th>
-					<th className="dow">Mo</th>
-					<th className="dow">Tu</th>
-					<th className="dow">We</th>
-					<th className="dow">Th</th>
-					<th className="dow">Fr</th>
-					<th className="dow">Sa</th>
+				<th className="dow">日</th>
+					<th className="dow">一</th>
+					<th className="dow">二</th>
+					<th className="dow">三</th>
+					<th className="dow">四</th>
+					<th className="dow">五</th>
+					<th className="dow">六</th>
 				</tr>
 			</thead>
 		);
