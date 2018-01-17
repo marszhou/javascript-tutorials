88749fe9ddee35ccf25c3a5b3f00b00e9bd13649
sunsy <sunshiyong8340@126.com>
2018-01-17 20:41:13 +0800

    start Bible project

---
+++ lesson-26/sunsy-bible/src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class BiblePanel extends Component {
+  render() {
+    return (
+      <div>
+        BiblePanel
+      </div>
+    );
+  }
+}
+
+export default BiblePanel;
