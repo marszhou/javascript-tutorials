e03b62f6732e6e961fe8db22bb35d96a34c6a922
sysun-PC\sysun <sunshiyong8340@126.com>
2018-01-18 14:50:25 +0800

    lesson-26 修改

--- lesson-26/sunsy-bible/src/Bible/BibleCatalogueBody.jsx
+++ lesson-26/sunsy-bible/src/Bible/BibleCatalogueBody.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import BibleChapter from './BibleChapter';
 
 class BibleCatalogueBody extends Component {
   render() {
--- lesson-26/sunsy-bible/src/Bible/BibleChapter.jsx
+++ lesson-26/sunsy-bible/src/Bible/BibleChapter.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import BibleSentence from './BibleSentence';
 
 class BibleChapter extends Component {
   render() {
