49fca7d24c3e3cf208f89d30f9b5bd7671174cb7
sysun-PC\sysun <sunshiyong8340@126.com>
2018-02-02 16:20:14 +0800

    no message

--- sunsy/201802/02/index.js
+++ sunsy/201802/02/index.js
@@ -1,18 +1,48 @@
 function longest(str){
   const arrayOfStr = str.split(" ");
   const longest = arrayOfStr.sort((a, b) => b.length - a.length)[0];
-  console.log(longest);
+  return longest;
 }
 
 function capitalWords(str){
   const arrayOfStr = str.toLowerCase().split(" ");
   const capitalWords = arrayOfStr.map(str => 
     str.replace(str.charAt(0), str.charAt(0).toUpperCase())).join(" ");
-  console.log(capitalWords);
+  return capitalWords;
 }
 
-function range(x, y, z){
-  return z === -1 ? [...Array(x-y+1)].map(() => x--) : [...Array(y-x+1)].map(() => x++)
+function capitalWords2(str){
+  const arrayOfStr = str.toLowerCase().split(" ");
+  const capitalWords = arrayOfStr.map(str => 
+    str[0].toUpperCase() + str.slice(1)
+  ).join(" ");
+  return capitalWords;
+}
+
+function range(a, b, n){
+  const x = a, y = b;
+  const step = n ? n : 1;
+  let i = 0;
+  const outPutArray = [];
+  const outPut = () => {
+    if(x < y){
+      if(x + i < y) {
+        outPutArray.push(x + i);
+        i += step;
+        outPut();
+      }
+    }
+    if(x > y){
+      if(x - i > y){
+        outPutArray.push(x - i);
+        i += step;
+        outPut();
+      }
+    }
+  }
+  outPut();
+  outPutArray.push(y);
+  return outPutArray;
 }
 
 function add(arr){
